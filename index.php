7bfbfeede8df627068ea1c7ca97dfb31c1b66950
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-01-24 17:36:36 +0100

    Bonus 1  - Multiple genres for movie

--- index.php
+++ index.php
@@ -1,35 +1,62 @@
 <?php
+    
+    class Genre {
+
+        public $genreName;
+
+        public function __construct($genreName){
+
+            $this -> genreName = $genreName;
+        }
+    }
+    
     class Movie {
 
         public $title;
-        public $genre;
+        public array $genres;
         public $author;
         public $releaseYear;
 
-        public function __construct($title, $genre, $author, $releaseYear){
+        public function __construct($title, $genres, $author, $releaseYear){
 
             $this -> title = $title;
-            $this -> genre = $genre;
+            $this -> genres = $genres;
             $this -> author = $author;
             $this -> releaseYear = $releaseYear;
         }
 
+        public function getFullGenres(){
+
+            $string = '';
+
+            foreach($this -> genres as $genre){
+
+                $string .= $genre -> genreName . ' ';
+            }
+
+            return $string;
+        }
+
         public function buildHtml(){
             echo '<h1>Movie: ' . $this -> title . '</h1>'
-                . '<p>' . $this -> genre . '</p>'
+                . '<p>' . $this -> getFullGenres() . '</p>'
                 . '<p>' . $this -> author. '</p>'
                 . '<p>' . $this -> releaseYear . '</p>';
-        }
+            }
+            
+            
     }
+        
+    $genres1 = [new Genre('Animazione'), new Genre('Commedia'), new Genre('Avventura'), new Genre('Fantascienza'), new Genre('Drammatico')];
+    $genres2 = [new Genre('Fantascienza'), new Genre('Thriller'), new Genre('Horror'), new Genre('Azione')];
+    $genres3 = [new Genre('Avventura'), new Genre('Azione'), new Genre('Commedia'), new Genre('Fantascienza'), new Genre('Horror')];
+    $genres4 = [new Genre('Fantastico'), new Genre('Drammatico'), new Genre('Avventura')];
 
-    $movie1 = new Movie('Il gigante di ferro', 'Animazione', 'Brad Bird', '1999');
-    $movie2 = new Movie('Alien' , 'Fantascienza', 'Ridley Scott', '1979');
-    $movie3 = new Movie('La Casa', 'Horror', 'Sam Raimi', '1981');
-    $movie4 = new Movie('Big Fish - Le storie di una vita incredibile', 'Fantastico', 'Tim Burton', '2003');
+    $movies = [new Movie('Il gigante di ferro', $genres1, 'Brad Bird', '1999'), new Movie('Alien' , $genres2, 'Ridley Scott', '1979'), new Movie("L'armata delle tenebre", $genres3, 'Sam Raimi', '1992'), new Movie('Big Fish - Le storie di una vita incredibile', $genres4, 'Tim Burton', '2003')];
 
-    echo $movie1 -> buildHtml();
-    echo $movie2 -> buildHtml();
-    echo $movie3 -> buildHtml();
-    echo $movie4 -> buildHtml();
+    foreach($movies as $movie){
+        
+         echo $movie -> buildHtml(); 
+    }
 
 ?>
